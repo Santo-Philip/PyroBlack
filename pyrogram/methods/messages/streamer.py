b732be8c2631f178539fec99c9006a5a0f89563e
Santo-Philip <santop7510@gmail.com>
2023-05-10 23:58:24 +0000

    Update streamer.py

--- pyrogram/methods/messages/streamer.py
+++ pyrogram/methods/messages/streamer.py
@@ -1,3 +1,4 @@
+import os
 import tempfile
 from typing import Union, AsyncGenerator
 
@@ -6,56 +7,74 @@ from pyrogram import types
 from pyrogram.file_id import FileId
 
 
-async def streamer(
-    self: "pyrogram.Client",
-    message: Union["types.Message", str],
-    chunk_size: int = 10240,
-) -> AsyncGenerator[bytes, None]:
-    available_media = (
-        "audio",
-        "document",
-        "photo",
-        "sticker",
-        "animation",
-        "video",
-        "voice",
-        "video_note",
-        "new_chat_photo",
-    )
+class StreamMediaMod:
 
-    if isinstance(message, types.Message):
-        for kind in available_media:
-            media = getattr(message, kind, None)
+    def __init__(self, client):
+        self.client = client
 
-            if media is not None:
-                break
+    async def streamer(
+        self,
+        message: Union["types.Message", str],
+        chunk_size: int = 10240,
+        delete_folder: bool = False,
+    ) -> AsyncGenerator[bytes, None]:
+        available_media = (
+            "audio",
+            "document",
+            "photo",
+            "sticker",
+            "animation",
+            "video",
+            "voice",
+            "video_note",
+            "new_chat_photo",
+        )
+
+      
+        if isinstance(message, types.Message):
+            for kind in available_media:
+                media = getattr(message, kind, None)
+
+                if media is not None:
+                    break
         else:
+            media = message
+
+       
+        if media is None:
             raise ValueError("This message doesn't contain any downloadable media")
-    else:
-        media = message
 
-    if isinstance(media, str):
-        file_id_str = media
-    else:
-        file_id_str = media.file_id
+      
+        if isinstance(media, str):
+            file_id_str = media
+        else:
+            file_id_str = media.file_id
 
-    file_id_obj = FileId.decode(file_id_str)
-    file_size = getattr(media, "file_size", 0)
+       
+        file_id_obj = FileId.decode(file_id_str)
 
-    # Create a temporary file.
-    tmp_file = tempfile.NamedTemporaryFile(delete=False)
+       
+        file_size = getattr(media, "file_size", 0)
 
-    # Start downloading the media in chunks.
-    offset = 0
-    while True:
-        chunk = await self.download_media(file_id_obj, offset=offset, limit=chunk_size)
-        if not chunk:
-            break
-        # Yield the chunk.
-        yield chunk
-        # Delete the chunk.
-        del chunk
-        offset += chunk_size
+        
+        tmp_file = tempfile.NamedTemporaryFile(delete=False)
 
-    # Close the temporary file.
-    tmp_file.close()
+      
+        offset = 0
+        while True:
+            chunk = await self.client.download_media(file_id_obj, offset=offset, limit=chunk_size)
+            if not chunk:
+                break
+         
+            yield chunk
+
+            del chunk
+            offset += chunk_size
+
+        tmp_file.close()
+
+        if delete_folder:
+            try:
+                os.rmdir(tmp_file.name)
+            except OSError:
+                pass
